0439fd578bc18b0cb6b533aa7f38be4d5fda3a2e
Ashenix01 <ashish.tech00@gmail.com>
2026-06-03 20:35:00 +0530

    teacher authentication

--- src/screens/teacher_dashboard.py
+++ src/screens/teacher_dashboard.py
@@ -2,13 +2,14 @@ import streamlit as st
 from src.UI.base_layout import style_base_layout_dashboard,style_base_layout_home,style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
-
+from src.database.db import check_teacher_exists,create_teacher,teachers_login
 
 def teacher_dashboard():
     style_base_layout_dashboard()
     style_base_layout()
-
-    if 'teacher_login_type' not in st.session_state or st.session_state['teacher_login_type']=='login':
+    if "teacher_data" in st.session_state:
+        teacher_dashboard_screen()
+    elif 'teacher_login_type' not in st.session_state or st.session_state['teacher_login_type']=='login':
         teacher_dashboard_login()
 
     elif st.session_state['teacher_login_type']=='register':
@@ -16,8 +17,20 @@ def teacher_dashboard():
         st.session_state['teacher_login_type']='register'
     
         
+def teacher_dashboard_screen():
+    teacher_data=st.session_state['teacher_data']
+    st.title("Teacher Dashboard")
+    st.header(f"Welcome {teacher_data['name']}! Here's your dashboard.")
 
-
+def login_teacher(username, password):
+    teacher=teachers_login(username, password)
+    if teacher:
+        st.session_state['teacher_data']=teacher
+        st.session_state['user_role']="teacher"
+        st.session_state['is_logged_in']=True
+        return True
+    else:
+        return False
 def teacher_dashboard_login():
     col1, col2=st.columns(2,gap="large")
     with col1:
@@ -43,9 +56,21 @@ def teacher_dashboard_login():
     btn_col1, btn_col2=st.columns(2, gap="small")
     with btn_col1:
         if st.button("Login", type='primary', key='teacherloginbtn',icon=":material/passkey:", icon_position="left",width='stretch'):
-            st.session_state['teacher_login_type']='login'
-            st.success("Login successful!")
-            st.rerun()
+            if not teacher_username or not teacher_password:
+                st.error("Please enter both username and password.")
+                return
+            if login_teacher(teacher_username, teacher_password):
+                st.toast("Login successful!", icon="✅")
+                import time
+                time.sleep(2)
+                st.rerun() # Rerun is important because Session States were changed when login_teacher() function was called
+
+            else:
+                st.error("Invalid username or password.")
+                import time
+                time.sleep(2)
+                st.session_state['teacher_login_type']='login'
+                st.rerun()
     with btn_col2:
         if st.button("Register Instead", type='secondary', key='teacherregisterbtn',icon=":material/person_add:", icon_position="left",width='stretch'):
             st.session_state['teacher_login_type']='register'
@@ -54,8 +79,21 @@ def teacher_dashboard_login():
     footer_dashboard()
 
 
+def register_teacher(teacher_name, subject_expertise, subject_code, teacher_username, teacher_password, teacher_confirm_password):
+    if not all([teacher_name, subject_expertise, subject_code, teacher_username, teacher_password, teacher_confirm_password]):
+        return False, "Please fill in all the fields."
 
+    if teacher_password != teacher_confirm_password:
+        return False, "Passwords do not match."
 
+    if check_teacher_exists(teacher_username):
+        return False, "Username already exists. Please choose a different username."
+    
+    try:
+        create_teacher(teacher_username, teacher_password, teacher_name,subject_expertise, subject_code)
+        return True, "Registration successful. Login with your credentials."
+    except Exception as e:
+        return False, f"Unexpected error occured during registration ! + {str(e)}"
 
 
 def teacher_dashboard_register():
@@ -86,10 +124,19 @@ def teacher_dashboard_register():
     st.divider()
     btn_col1, btn_col2=st.columns(2, gap="small")
     with btn_col1:
-        if st.button("Register", type='secondary', key='teacherloginbtn',icon=":material/passkey:", icon_position="left",width='stretch'):
-            st.session_state['teacher_login_type']='login'
-            st.success("Registration successful! Please login with your credentials.")
-            st.rerun()
+        if st.button("Register Now!", type='secondary', key='teacherloginbtn',icon=":material/passkey:", icon_position="left",width='stretch'):
+            success, message = register_teacher(teacher_name, subject_expertise, subject_code, teacher_username, teacher_password, teacher_confirm_password)
+            if success:
+                st.toast("Registration successful!", icon="✅")
+                import time
+                time.sleep(2)
+                st.session_state['teacher_login_type']='login'
+                st.rerun()
+            else:
+                st.error(message)
+                import time
+                time.sleep(2)
+
     with btn_col2:
         if st.button("Login Instead", type='primary', key='teacherregisterbtn',icon=":material/person_add:", icon_position="left",width='stretch'):
             st.session_state['teacher_login_type']='login'
