dfaa53a72eda2b7204849640143a8ce19b3888b9
Ashenix01 <ashish.tech00@gmail.com>
2026-06-04 18:48:28 +0530

    installed new voice recognision and preprocessing library and face recognition ml pipeline done

---
+++ src/pipelines/face_pipeline.py
@@ -0,0 +1,106 @@
+import dlib
+import face_recognition_models
+from sklearn.svm import SVC
+import numpy as np
+import streamlit as st
+from src.database.db import get_all_students
+
+
+@st.cache_resource #So that model load for only one time
+def load_dlib_models():
+    detector=dlib.get_frontal_face_detector()
+
+    sp=dlib.shape_predictor(
+        face_recognition_models.pose_predictor_model_location()
+    )
+
+    facerec=dlib.face_recognition_model_v1(
+        face_recognition_models.face_recognition_model_location()
+    )
+    return detector,sp,facerec 
+
+
+def get_face_embeddings(image_np):
+    detector,sp,facerec=load_dlib_models()
+
+    faces=detector(image_np,1)
+
+    embeddings=[]
+
+    for face in faces:
+        shape = sp(image_np,face) # gives 68 landmark point of the face 
+        face_description = facerec.compute_face_descriptor(image_np,shape,1) # Genereate 128 Dimension embedding for the image Unique signature of the face
+        embeddings.append(np.array(face_description))
+
+    return embeddings
+
+
+@st.cache_resource
+def get_trained_model():
+    X=[]  #all face embeddings of in numpy array form
+    y=[]  #all students ids to uniquely identity all students
+
+    students_db=get_all_students()
+
+    if not students_db:
+        return None
+    for student in students_db:
+        embedding=student.get('face_embedding')
+        id=student.get('student_id')
+        X.append(np.array(embedding))
+        y.append(id)
+    
+    if len(X)==0:
+        return 0
+    
+    clf=SVC(kernel='linear',probability=True,class_weight='balanced')
+
+    try:
+        clf.fit(X,y)
+    except ValueError:
+        pass
+    return {
+        "clf":clf,
+        "X":X,
+        "y":y
+    }
+
+def train_classifier():
+    st.cache_resource.clear()
+    model_data=get_trained_model() 
+    return bool(model_data)
+
+def predict_attendance(class_image_np):
+    embeddings=get_face_embeddings(class_image_np)
+    detected_students={}
+    model_data = get_trained_model()
+
+    if not model_data:
+        return detected_students,[],len(embeddings)
+    
+    clf = model_data['clf']
+    X_train=model_data['X']
+    y_train=model_data['y']
+
+    all_students=sorted(list(set(y_train)))
+
+    for embedding in embeddings:
+        if len(all_students)>=2:
+            predicted_id=int(clf.predict([embedding]))
+        else:
+            predicted_id=int(all_students[0])
+        
+        student_embedding=X_train[y_train.index(predicted_id)]
+        best_match_score = np.linalg.norm(student_embedding-embedding)
+        resemblance_threshold = 0.6
+        if best_match_score <= resemblance_threshold:
+            detected_students[predicted_id]=True
+
+    return detected_students,all_students,len(embeddings)
+
+
+
+    
+
+
+    
--- src/screens/student_dashboard.py
+++ src/screens/student_dashboard.py
@@ -1,7 +1,35 @@
 import streamlit as st
+from src.UI.base_layout import style_base_layout_dashboard,style_base_layout_home,style_base_layout
+from src.components.header import header_dashboard
+from src.components.footer import footer_dashboard
+from src.database.db import check_teacher_exists,create_teacher,teachers_login
+from PIL import image
+import numpy as np
 
 def student_dashboard():
-    st.title("Student Dashboard")
-    st.header("Welcome to your dashboard")
+    style_base_layout_dashboard()
+    style_base_layout()
+    col1, col2=st.columns(2,gap="large")
+    with col1:
+        header_dashboard()
+    with col2:
+        st.markdown(
+            "<div style='margin-top:60px; display:flex; justify-content:center;'>",
+            unsafe_allow_html=True
+        )
+        if st.button("Go Back to Home", type='secondary', key='loginbackbtn', icon=":material/arrow_back:", icon_position="left"):
+            st.session_state['login_type'] = None
+            st.rerun()
+
+        st.markdown("</div>", unsafe_allow_html=True)
+
+    st.space(2)
+
+    st.header("Login using FaceID")
+    photo_source=st.camera_input("Position your face in Center!")
+    if photo_source:
+        np.array(image.open(photo_source))
+    footer_dashboard()
+    
 
     
